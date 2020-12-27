445298be5dab0a6155bcd23c88a39bf3f4386e90
Kseniiiiia <kseniia.pashkoff@gmail.com>
2020-12-27 15:25:12 +0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -2,3 +2,7 @@ print('Hello, what is your name?')
 name = input('Hello, what is your name?')
 
 print('Hello', name)
+year= input('When you was born?'  )
+print('nice year', year)
+
+print('Your age', 2020-int(year))
