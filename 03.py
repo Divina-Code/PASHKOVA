6b46176d71c275bcaa41c3e85fc3c9ee554a0576
Kseniiiiia <kseniia.pashkoff@gmail.com>
2020-12-27 17:48:02 +0300

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,12 @@
+print('Hello, what is your name?')
+name = input('Hello, what is your name?')
+
+print('Hello', name)
+year= input('When you was born?'  )
+print('Your age', 2020-int(year))
+
+if len(str(year))>18:
+    print('Посмотрите боевик')
+else:
+    print('Посмотрите комедию')
+
