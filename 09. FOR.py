7125330cb1813bc7194d75f1dc3a25bf26bd690c
Kseniiiiia <kseniia.pashkoff@gmail.com>
2020-12-29 16:06:28 +0300

    Update 09. FOR.py

--- 09. FOR.py
+++ 09. FOR.py
@@ -5,7 +5,7 @@ print(words)
 
 for word in words:
     word1 = word[::-1].lower()
-    if word == word1:
+    if word.lower() == word1:
         print("Это палиндром")
     else:
         print("Это не палиндром")
